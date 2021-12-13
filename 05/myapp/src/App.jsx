7af93dca252ef3a4eb3a617773f78a4da858f064
Paulo Roberto Abrahão de Souza <ab.cod@outlook.com>
2021-12-13 15:52:46 -0300

    att 05

--- 05/myapp/src/App.jsx
+++ 05/myapp/src/App.jsx
@@ -1,16 +1,28 @@
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+import Container from '@mui/material/Container';
 
 import './style.css';
 
 function App() {
   
   return (
-    <div className="exemplo">
-      <TextField id="outlined-basic" label="Nome" variant="outlined" className="nome"/>
+    <>
+      <div className="exemplo">
 
-      <TextField id="outlined-basic" label="Senha" variant="outlined" type="password"/>
-    </div>
+        <h1 style={{fontFamily:'Roboto', fontWeight: '300', fontSize: '1.6rem'}}>Sign in</h1>
+
+        <TextField id="outlined-basic" label="Name" variant="outlined" className="name"/>
+
+        <TextField id="outlined-basic" label="Password" variant="outlined" type="password" className="password"
+          sx={{ marginTop: '1.5rem'}}
+        />          
+
+        <Button variant="contained" className="signIn" sx={{marginTop: '1.5rem', width: '30%'}}>
+          SIGN IN
+        </Button>
+      </div>
+    </>
   );
 }
 
