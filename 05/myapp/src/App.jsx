2bd42e329553a2ff6e8115644038ab9afa65d159
Paulo Roberto Abrahão de Souza <ab.cod@outlook.com>
2021-12-13 15:12:08 -0300

    envio arq 05

--- 05/myapp/src/App.jsx
+++ 05/myapp/src/App.jsx
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
+import './style.css';
 
 function App() {
+  
   return (
-    <>
-    </>
+    <div className="exemplo">
+      <TextField id="outlined-basic" label="Nome" variant="outlined" className="nome"/>
+
+      <TextField id="outlined-basic" label="Senha" variant="outlined" type="password"/>
+    </div>
   );
 }
 
