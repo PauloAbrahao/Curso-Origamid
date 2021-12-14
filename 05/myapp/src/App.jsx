4aed4fd7acbc81c95fc5f290e485fb5b3b449d07
Paulo Roberto Abrahão de Souza <ab.cod@outlook.com>
2021-12-14 12:22:25 -0300

    att 05/06

--- 05/myapp/src/App.jsx
+++ 05/myapp/src/App.jsx
@@ -1,6 +1,7 @@
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import Container from '@mui/material/Container';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
 
 import './style.css';
 
@@ -10,7 +11,7 @@ function App() {
     <>
       <div className="exemplo">
 
-        <h1 style={{fontFamily:'Roboto', fontWeight: '300', fontSize: '1.6rem'}}>Sign in</h1>
+        <h1 style={{fontFamily:'Roboto', fontWeight: '400', fontSize: '1.5rem'}}>Sign in</h1>
 
         <TextField id="outlined-basic" label="Name" variant="outlined" className="name"/>
 
@@ -18,9 +19,19 @@ function App() {
           sx={{ marginTop: '1.5rem'}}
         />          
 
-        <Button variant="contained" className="signIn" sx={{marginTop: '1.5rem', width: '30%'}}>
+        <div className="checkbox">
+          <FormControlLabel control={<Checkbox />} label="Remember me" />
+        </div>
+
+        <Button variant="contained" className="signIn" sx={{marginTop: '1.2rem', width: '30%'}}>
           SIGN IN
         </Button>
+
+        <div className="forgotCreate">
+          <a> Forgot password? </a>
+          <a> Don't have an account? Sign Up </a>
+        </div>
+
       </div>
     </>
   );
---
+++ 05/myapp2/src/App.jsx
@@ -0,0 +1,28 @@
+import "./App.css";
+import "./style.css";
+import Slide from "./Slide";
+
+function App() {
+  const slides = [
+    {
+      id: "slide1",
+      text: "Slide 1",
+    },
+    {
+      id: "slide2",
+      text: "Slide 2",
+    },
+    {
+      id: "slide3",
+      text: "Slide 3",
+    },
+  ];
+
+  return (
+    <>
+      <Slide slides={slides} />
+    </>
+  );
+}
+
+export default App;
