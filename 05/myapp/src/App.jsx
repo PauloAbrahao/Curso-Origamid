243ebcd184106cc61d3664b243df5eb0da9c66e9
Paulo Roberto Abrahão de Souza <ab.cod@outlook.com>
2021-12-15 20:20:16 -0300

    estudo sobre MUI

--- 05/myapp/src/App.jsx
+++ 05/myapp/src/App.jsx
@@ -1,29 +1,60 @@
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Checkbox from '@mui/material/Checkbox';
-import FormControlLabel from '@mui/material/FormControlLabel';
+import React from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Checkbox from "@mui/material/Checkbox";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import InputAdornment from "@mui/material/InputAdornment";
+import InputLabel from "@mui/material/InputLabel";
+import IconButton from "@mui/material/IconButton";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import OutlinedInput from "@mui/material/OutlinedInput";
 
-import './style.css';
+import "./style.css";
 
 function App() {
-  
+  const [nome, setNome] = React.useState("");
+  const [pass, setPass] = React.useState("");
+
   return (
     <>
       <div className="exemplo">
+        <h1
+          style={{
+            fontFamily: "Roboto",
+            fontWeight: "400",
+            fontSize: "1.5rem",
+          }}
+        >
+          Sign in
+        </h1>
 
-        <h1 style={{fontFamily:'Roboto', fontWeight: '400', fontSize: '1.5rem'}}>Sign in</h1>
+        <TextField
+          id="outlined-basic"
+          label="Name"
+          variant="outlined"
+          className="name"
+        />
 
-        <TextField id="outlined-basic" label="Name" variant="outlined" className="name"/>
-
-        <TextField id="outlined-basic" label="Password" variant="outlined" type="password" className="password"
-          sx={{ marginTop: '1.5rem'}}
-        />          
+        <TextField
+          id="outlined-basic"
+          label="Password"
+          variant="outlined"
+          className="password"
+          type="password"
+          sx={{ marginTop: "1.5rem" }}
+        />
 
         <div className="checkbox">
           <FormControlLabel control={<Checkbox />} label="Remember me" />
         </div>
 
-        <Button variant="contained" className="signIn" sx={{marginTop: '1.2rem', width: '30%'}}>
+        <Button
+          variant="contained"
+          className="signIn"
+          sx={{ marginTop: "1.2rem", width: "30%" }}
+        >
           SIGN IN
         </Button>
 
@@ -31,8 +62,9 @@ function App() {
           <a> Forgot password? </a>
           <a> Don't have an account? Sign Up </a>
         </div>
-
       </div>
+
+   
     </>
   );
 }
